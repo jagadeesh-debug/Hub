f4f879e93c279ad072f9713e572563e914d6bba2
jagadeesh-debug <jaggurockz4892@gmail.com>
2024-11-24 17:26:09 +0530

    user can now change profile dynamically

--- Recycle_Hub/src/components/User_Account.jsx
+++ Recycle_Hub/src/components/User_Account.jsx
@@ -4,21 +4,66 @@ import {db, auth} from "../Backend/firebaseconfig";
 import { getDoc, doc, setDoc, getFirestore } from "firebase/firestore";
 export default function User_acc() {
     const [mobile, setMobile] = useState("");
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [address, setAddress] = useState("");
+    const db = getFirestore(); 
+   
+    const handleEditName= async () => {
+        const newName = prompt("Enter new name:");
+        if (newName) {
+            setName(newName);
+            const userDoc = doc(db, "users", auth.currentUser.uid);
+            await setDoc(userDoc, { name: newName }, { merge: true });
+        }
+    };
 
-    const db = getFirestore();
-    const updateMobile=async (e)=>{
-        e.preventDefault();
-    }    
-
+    const handleEditMobile = async () => {
+        const newMobile = prompt("Enter new mobile number:");
+        if (newMobile) {
+            setMobile(newMobile);
+            const userDoc = doc(db, "users", auth.currentUser.uid);
+            await setDoc(userDoc, { mobile: newMobile }, { merge: true });
+        }
+    };
+    const handleEditEmail = async ()=>{
+        const newmail = prompt("Enter new mail");
+        if(newmail){
+            setEmail(newmail);
+            const userDoc = doc(db, "users", auth.currentUser.uid);
+            await setDoc(userDoc, { email: newmail }, { merge: true }); 
+        }
+    };
+    const handleEditAddress = async ()=>{
+        const newAddress = prompt("Enter new address");
+        if(newAddress){
+            setAddress(newAddress);
+            const userDoc = doc(db, "users", auth.currentUser.uid);
+            await setDoc(userDoc, { address: newAddress }, { merge: true });
+        }
+    };
+    useEffect(() => {
+        const fetchUserData = async () => {
+            const userDoc = doc(db, "users", auth.currentUser.uid);
+            const docSnap = await getDoc(userDoc);
+            if (docSnap.exists()) {
+                setName(docSnap.data().name);
+                setMobile(docSnap.data().mobile);
+                setEmail(docSnap.data().email);
+                setAddress(docSnap.data().address); 
+            }
+        };
+        fetchUserData();
+    }, []);
 
     return (
         <div className="h-screen  flex flex-col items-center ">
             <div className="= h-1/4 w-full md:w-1/2  md:mt-4 flex  md:justify-evenly">
                     <div className=" w-2/3 md:w-1/2 lg:w-1/3 flex  justify-center items-center">
-                        <img className="border border-black aspect-square  rounded-full  w-2/3 sm:w-1/2 md:w-3/4"  src="./src/assets/sin.jpg"/></div>
+                        <img className="border border-black aspect-square  rounded-full  w-2/3 md:w-2/3"  src="./src/assets/sin.jpg"/></div>
                     <div className="= w-5/6 md:w-1/2 flex flex-col items-center justify-center">
-                    <div className="  text-orange-400 md:text-xl lg:text-2xl" id="userAccName" style={{ fontFamily: 'Bagel Fat One, sans-serif' }}>Hello Ramanujan010</div>
-                    <div className="  text-black-400 md:text-md lg:text-xl" id="userName" style={{ fontFamily: 'Bagel Fat One, sans-serif' }}>Ramanujan</div>
+                    <div className="  text-orange-400 md:text-xl lg:text-2xl" id="userAccName" style={{ fontFamily: 'Bagel Fat One, sans-serif' }}>Hello {name}</div>
+                    <div className="  text-black-400 md:text-md lg:text-xl" id="userName" style={{ fontFamily: 'Bagel Fat One, sans-serif' }}>{name}</div>
                     <div className="flex = mt-2 w-full h-1/3  items-center justify-evenly">
                         <div className="flex flex-col items-center cursor-pointer">
                             <i class='bx bxs-package text-xl md:text-xl lg:text-3xl '></i>
@@ -42,30 +87,30 @@ export default function User_acc() {
                 <div className="flex  w-full h-1/6 justify-around items-center" id="details">
                     <div className="flex flex-col w-full md:w-1/2 ">
                         <h2 className="text-xl " style={{ fontFamily: 'Bagel Fat One, sans-serif' }} >Name</h2>
-                        <p className="text-xl font-semibold">Jagadeesh</p>
+                        <p className="text-xl font-semibold" id="User_name">{name}</p>
                     </div>
-                    <button className="text-xl italic bg-green-400 text-black w-1/4 h-fit rounded-md "><i class='bx bx-edit' ></i></button>
+                    <button className="text-xl italic bg-green-400 text-black w-1/4 h-fit rounded-md " onClick={handleEditName}><i class='bx bx-edit' id="edit" ></i></button>
                 </div>
                 <div className="flex  h-1/6 justify-around items-center" id="details">
                     <div className="flex flex-col w-full md:w-1/2">
-                        <h2 className="text-xl " style={{ fontFamily: 'Bagel Fat One, sans-serif' }} ></h2>
+                        <h2 className="text-xl " style={{ fontFamily: 'Bagel Fat One, sans-serif' }} >Mobile</h2>
                         <p className="text-xl font-semibold">{mobile}</p>
                     </div>
-                    <button className="text-xl italic bg-green-400 text-black w-1/4 h-fit rounded-md "><i class='bx bx-edit' ></i></button>
+                    <button className="text-xl italic bg-green-400 text-black w-1/4 h-fit rounded-md" onClick={handleEditMobile}><i class='bx bx-edit' ></i></button>
                 </div>
                 <div className="flex  h-1/6 justify-around items-center" id="details">
                     <div className="flex flex-col w-full md:w-1/2 ">
                         <h2 className="text-xl" style={{ fontFamily: 'Bagel Fat One, sans-serif' }} >Email</h2>
-                        <p className="text-xl font-semibold">Idiot@gmail.com</p>
+                        <p className="text-xl font-semibold">{email}</p>
                     </div>
-                    <button className="text-xl italic bg-green-400 text-black w-1/4 h-fit rounded-md "><i class='bx bx-edit' ></i></button>
+                    <button className="text-xl italic bg-green-400 text-black w-1/4 h-fit rounded-md " onClick={handleEditEmail}><i class='bx bx-edit' ></i></button>
                 </div>
                 <div className="flex  h-1/6 justify-around items-center" id="details">
                     <div className="flex flex-col w-full md:w-1/2">
                         <h2 className="text-xl " style={{ fontFamily: 'Bagel Fat One, sans-serif' }} >Address</h2>
-                        <p className="text-xl font-semibold">flat-no:143</p>
+                        <p className="text-xl font-semibold">{address}</p>
                     </div>
-                    <button className="text-xl italic bg-green-400 text-black w-1/4 h-fit rounded-md "><i class='bx bx-edit' ></i></button>
+                    <button className="text-xl italic bg-green-400 text-black w-1/4 h-fit rounded-md " onClick={handleEditAddress}><i class='bx bx-edit' ></i></button>
                 </div>
 
             </div>
